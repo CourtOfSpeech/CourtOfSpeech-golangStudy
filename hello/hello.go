ef59d596dc3bc76ee37410fa9fe6815c79a3acc6
xinjiang <1220052209@qq.com>
2020-03-23 15:50:23 +0800

    code commit

---
+++ hello/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type student struct {
+	name  string
+	phone int
+	addr  string
+}
+
+func main() {
+	fmt.Println("hello world!")
+
+	student := student{}
+	student.name = "jiangxin"
+	student.phone = 18328753072
+	student.addr = "四川绵阳三台县"
+	fmt.Println(student.name)
+	fmt.Println(student)
+
+}
