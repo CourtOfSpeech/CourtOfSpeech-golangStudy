bb02b787ab03e9e003075f90a73559f309a0ed14
Clover <1220052209@qq.com>
2020-03-23 16:08:58 +0800

    Update hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -17,10 +17,7 @@ func main() {
 	student.addr = "四川绵阳三台县"
 	fmt.Println(student.name)
 	fmt.Println(student.phone)
-<<<<<<< HEAD
 	fmt.Println(student.addr)
-=======
->>>>>>> fead5424d383b9d340c7f70b5214a963e9840865
 	fmt.Println(student)
 
 }
